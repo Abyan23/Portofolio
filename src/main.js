bc8e9b6bed5507e27b268e3a55d08dfe4799529e
Abyan23 <32361200+Abyan23@users.noreply.github.com>
2025-09-10 21:39:30 +0700

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -11,15 +11,15 @@ const swiper = new Swiper('.mySwiper', {
   },
 });
 
-// let lastScroll = 0;
-// window.addEventListener("scroll", () => {
-//   const current = window.pageYOffset;
-//   if (current > lastScroll) {
-//           nav.classList.add("translate-y-full","duration-300");
-//           nav.classList.remove("translate-y-0");
-//   } else {
-//           nav.classList.add("translate-y-0");
-//           nav.classList.remove("translate-y-full");
-//   }
-//   lastScroll = current;
-// });
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  const current = window.pageYOffset;
+  if (current > lastScroll) {
+          nav.classList.add("translate-y-full","duration-300");
+          nav.classList.remove("translate-y-0");
+  } else {
+          nav.classList.add("translate-y-0");
+          nav.classList.remove("translate-y-full");
+  }
+  lastScroll = current;
+});
